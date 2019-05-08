6dab4b7ac8a3b7beb6766d7d71ba3ab310d29488
Anthony Capili <acapili07@gmail.com>
2019-05-08 01:05:34 -0700

    feature-set-2 completed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './header.scss';
+import Nav from '../nav';
+import headerPic from '../../assets/images/header.png';
+import logoPic from '../../assets/images/logo.png';
 
 const Header = () => {
     return (
         <div>
-            <h1 className = "center">This is the header</h1>
+            <div className="header-pic">
+                <img src={headerPic}/>
+            </div>
+            <div>
+                <Nav></Nav>
+            </div> 
+            <div className="center">
+                <img src={logoPic}/>
+            </div>
+            {/* <h1 className = "center">This is the header</h1> */}
+            <div className="fancy center">
+                <h2>We deliver cupcakes for the important events in your life!</h2>
+            </div>
+            <div className="header-bar">
+
+            </div>
         </div>
     )
 }
